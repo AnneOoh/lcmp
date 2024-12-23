d5451bb34e41cae1145e0b381b6b05f7b6549189
MoeWhale <51768867+moewhale@users.noreply.github.com>
2024-12-23 14:43:57 +0800

    Update lcmp.sh

--- lcmp.sh
+++ lcmp.sh
@@ -443,7 +443,7 @@ _info "/usr/bin/mariadb -uroot -p 2>/dev/null < /data/www/default/pma/sql/create
 # 生成 22位 僅包含字母與數字的字串
 PMA_Random_String = $(head -c 100 /dev/urandom | tr -dc 'abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789_' | fold -w 22 | head -n 1)
 # 更改目錄名稱
-_error_detect mv /data/www/default/pma/ /data/www/default/${PMA_Random_String}/"
+_error_detect "mv /data/www/default/pma/ /data/www/default/${PMA_Random_String}/"
 _info "Set MariaDB completed"
 
 if check_sys rhel; then
